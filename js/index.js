0d97a49d310afc6da7fd1bc8003265a1988933e4
Jonathan Strickland <jonathan.strickland@gmail.com>
2021-03-11 19:01:21 -0600

    a few working events, will save for now

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+document.querySelector(".logo-heading").addEventListener("click", function (event) {
+    alert('Welcome!');
+});
+
+document.querySelector(".nav-link").addEventListener("mouseover", function (event) {
+    alert('You are Here');
+});
+document.querySelector(".nav-link").nextElementSibling.addEventListener("mouseover", function (event) {
+    alert('You are Here');
+});
+// ^Could keep doing this, but I think the idea is had.
+document.querySelector(".content-section").addEventListener("dblclick", function (event) { 
+    event.target.style.color = 'blue'; 
+});
+
+
+document.querySelector(".inverse-content").addEventListener("click", function (event) { 
+    event.target.style.background = 'orange'; 
+    event.target.style.color = 'white';
+});
+
+window.addEventListener('resize', function (event) {
+    alert('Hey! Quit messing with that!');
+    // document.style.background = 'red';
+});
+
+// var x = document.getElementById("mySelect");
+// x.remove(x.selectedIndex); 
+
+document.querySelector(".footer").addEventListener("dblclick", function (event) { 
+    event.target.style.background = 'pink' ; 
+});
